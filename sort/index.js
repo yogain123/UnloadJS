09ff1e1bba8e9cd0dc0db0083760c7d1779be508
yogendra saxena <yogendrasaxena@Admins-MacBook-Pro-4.local>
2019-09-24 15:34:23 +0530

    fix for sort

--- sort/index.js
+++ sort/index.js
@@ -1,5 +1,5 @@
 function sort(data, order = 1) {
-  if (Object.prototype.toString.call(data) == "object Array") {
+  if (Object.prototype.toString.call(data) == "[object Array]") {
     if (order === 1) {
       return data.sort((a, b) => a - b);
     } else if (order === -1) {
@@ -7,7 +7,7 @@ function sort(data, order = 1) {
     }
   }
 
-  if (Object.prototype.toString.call(data) == "object String") {
+  if (Object.prototype.toString.call(data) == "[object String]") {
     if (order === 1) {
       return data
         .split("")
